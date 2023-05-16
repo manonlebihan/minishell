6c33354ff65b21342e181acb3cc7912870afd3e9
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-05-16 16:02:36 +0200

    commit 3

---
+++ srcs/parser.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:59:25 by mle-biha          #+#    #+#             */
+/*   Updated: 2023/05/16 16:02:28 by mle-biha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	syntax_io_redirect(t_token **tokens, int *i, int *rc)
+{
+	if (!*rc)
+		return ;
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_WORD)
+		(*i)++;
+	else // syntax error
+		*rc = 0;
+}
+
+void	syntax_redirects(t_token **tokens, int *i, int *rc)
+{
+	if (!*rc)
+		return ;
+	syntax_io_redirect(tokens, i, rc);
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_REDIRECT)
+	{
+		(*i)++;
+		syntax_redirects(tokens, i, rc);
+	}
+}
+
+void	syntax_simple_cmd(t_token **tokens, int *i, int *rc)
+{
+	if (!*rc)
+		return ;
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_WORD)
+	{
+		(*i)++;
+		while (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_WORD)
+			(*i)++;
+	}
+	else // syntax error
+		*rc = 0;
+}
+
+void	syntax_cmd(t_token **tokens, int *i, int *rc)
+{
+	if (!*rc)
+		return ;
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_REDIRECT)
+	{
+		(*i)++;
+		syntax_redirects(tokens, i, rc);
+	}
+	else
+		syntax_simple_cmd(tokens, i, rc);
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_REDIRECT)
+	{
+		(*i)++;
+		syntax_redirects(tokens, i, rc);
+	}
+}
+
+void	syntax_minishell_cmd(t_token **tokens, int *i, int *rc)
+{
+	if (!*rc)
+		return ;
+	syntax_cmd(tokens, i, rc);
+	if (tokens[*i] && tokens[*i]->type == TOKEN_TYPE_PIPE)
+	{
+		(*i)++;
+		syntax_minishell_cmd(tokens, i, rc);
+	}
+}
+
+int	parse(t_token **tokens)
+{
+	int	rc;
+	int	i;
+
+	i = 0;
+	rc = 1;
+	syntax_minishell_cmd(tokens, &i, &rc);
+	return (rc);
+}

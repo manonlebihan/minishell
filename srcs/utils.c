65f416bbaa360b44da3b2f89294aec2877430b74
Manon Le Bihan <manonlebihan@MBP-de-Manon.lan>
2023-05-22 15:42:57 +0200

    commit 45

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,11 +6,36 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 15:03:44 by mle-biha          #+#    #+#             */
-/*   Updated: 2023/05/22 15:04:00 by mle-biha         ###   ########.fr       */
+/*   Updated: 2023/05/22 15:42:03 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	divide_count_words(char *string, int *nb, int *prev_was_sep)
+{
+	if (*string == '"')
+	{
+		*string += 1;
+		while (*string && *string++ != '"')
+			;
+		*nb += 1;
+	}
+	else if (*string == '\'')
+	{
+		*string += 1;
+		while (*string && *string++ != '\'')
+			;
+		*nb += 1;
+	}
+	else if (*string == ' ')
+		*prev_was_sep = 1;
+	else if (*string != ' ' && *prev_was_sep)
+	{
+		*nb += 1;
+		*prev_was_sep = 0;
+	}
+}
 
 int	count_words(char *string)
 {
@@ -23,27 +48,7 @@ int	count_words(char *string)
 	{
 		if (*string == '|' || *string == '<' || *string == '>')
 			nb++;
-		else if (*string == '"')
-		{
-			string++;
-			while (*string && *string++ != '"')
-				;
-			nb++;
-		}
-		else if (*string == '\'')
-		{
-			string++;
-			while (*string && *string++ != '\'')
-				;
-			nb++;
-		}
-		else if (*string == ' ')
-			prev_was_sep = 1;
-		else if (*string != ' ' && prev_was_sep)
-		{
-			nb++;
-			prev_was_sep = 0;
-		}
+		divide_count_words(string, &nb, &prev_was_sep);
 		string++;
 	}
 	return (nb);
